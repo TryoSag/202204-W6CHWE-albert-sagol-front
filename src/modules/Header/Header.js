2b2e3e7d598748df93fefdf76757b9dd09298c79
Albert <albert.sagol@gmail.com>
2022-05-16 23:21:58 +0200

    added style component

--- src/modules/Header/Header.js
+++ src/modules/Header/Header.js
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  width: 100wv;
+  display: flex;
+  justify-content: center;
+  background-color: #f6f6f6;
+
+  h1 {
+    display: flex;
+  }
+`;
+
 const Header = () => {
-  return <h1>Best Robots</h1>;
+  return (
+    <HeaderContainer>
+      <h1>Robots</h1>
+    </HeaderContainer>
+  );
 };
 
 export default Header;
