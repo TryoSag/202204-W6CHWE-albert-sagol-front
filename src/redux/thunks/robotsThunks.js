3456c5eadac7bf3f313eea40f7ad3ff0d2bbdc71
AlbSag <92945087+TryoSag@users.noreply.github.com>
2022-05-17 13:56:21 +0200

    fixed naming header

--- src/redux/thunks/robotsThunks.js
+++ src/redux/thunks/robotsThunks.js
@@ -2,15 +2,11 @@ import axios from "axios";
 import { loadRobotsActionCreator } from "../features/robotsSlice";
 
 export const loadRobotsThunk = () => async (dispatch) => {
-  const { data: robots } = await axios.get(
-    "https://two02204-w6chwe-albert-sagol-back.onrender.com/",
-    {
-      header: {
-        authoritzation:
-          "Bearer 5E884898DA28047151D0E56F8DC6292773603D0D6AABBDD62A11EF721D1542D8",
-      },
-    }
-  );
+  const { data: robots } = await axios.get(process.env.API_STRING, {
+    header: {
+      authoritzation: `Bearer ${process.env.TOKEN_STRING}`,
+    },
+  });
 
   dispatch(loadRobotsActionCreator(robots));
 };
